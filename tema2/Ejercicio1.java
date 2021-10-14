bf82b7f3aa02e7cf16840b1d8393875adc0de9f3
davidservia <david.servia.fp@iescampanillas.com>
2021-10-14 10:47:02 +0200

    ejercicios1_del_tema2

---
+++ tema2/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+  public static void main (String[] args) {
+  int x = 144;
+  int y = 999;
+  System.out.println(x);
+  System.out.println(y);
+  System.out.println(x + y);
+  System.out.println(x * y);
+  System.out.println((double)x / (double)y);
+  }
+}
---
+++ tema2/Ejercicio2.java
@@ -0,0 +1,6 @@
+public class Ejercicio2 {
+  public static void main (String[] args) {
+  char letra = 'David';
+  System.out.println(letra);
+  }
+}
